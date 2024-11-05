efa476f54f594cd4e8f3648c8d2c9b0fe9abe7d8
Troupote <73849862+Troupote@users.noreply.github.com>
2024-11-05 10:36:47 +0100

    Ajoutez des fichiers projet.

---
+++ De.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Jeux421
+{
+    internal class De
+    {
+
+        public string NbFaces;
+        private Random random;
+        public int Face { get; private set; }
+
+        public De(string nbFaces = "6")
+        {
+            NbFaces = nbFaces;
+            random = new Random();
+        }
+
+        public override string ToString()
+        {
+            string toString = "+---+\n";
+            toString += $"| {this.Face} |\n";
+            toString += "+---+\n";
+            return toString;
+        }
+
+        public int Lancer()
+        {
+            this.Face = this.random.Next(1, int.Parse(NbFaces) + 1);
+            return this.Face;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jeux421
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, monde!");
+        }
+    }
+}
